891653621d4d4569017c21e8ddc502c0547edf43
poyonosk <117244700+poyonosk@users.noreply.github.com>
2022-12-22 10:46:11 +0000

    [Finish]bookers2kadai3

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "Book was successfully created !"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
