5ee3279de4aa42f383ed0e19c90b22b2c9136fad
poyonosk <117244700+poyonosk@users.noreply.github.com>
2022-12-24 09:53:40 +0000

    [Finish]bookers2kadai3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :collect_user, only: [:edit, :update, :destroy]
+
   def index
     @book = Book.new
     @books = Book.all
@@ -12,13 +14,15 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created !"
       redirect_to book_path(@book.id)
     else
-      render :edit
+      @books = Book.all
+      @user = current_user
+      render :index
     end
   end
 
   def show
     @book = Book.find(params[:id])
-    @user = current_user
+    @user = @book.user
   end
 
   def edit
@@ -26,6 +30,13 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated !"
+      redirect_to book_path
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -38,5 +49,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def collect_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :collect_user, only: [:edit, :update]
+
   def index
     @users = User.all
     @user = current_user
@@ -7,14 +9,14 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "info was successfully updated !"
       redirect_to user_path
     else
-      redirect_to edit_user_path
+      render :edit
     end
   end
 
@@ -22,9 +24,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def collect_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
